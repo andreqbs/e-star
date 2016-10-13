38cec85f2ec76c8ec283f22e5c0a680d7ed0d2ad
EricoIgor <erick.igor@outlook.com>
2016-10-13 17:49:21 -0300

    Adição do controler geral e alguns métodos e a classe modelo de Componentes junto com o respectivo DAO

---
+++ App/Controllers/generalController.php
@@ -0,0 +1,25 @@
+<?php 
+
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+use App\Models\TCC as TCC;
+use App\DAO\DAOTCC as DAOTCC;
+require_once dirname(__FILE__).'/../Model/TCC.php';
+require_once dirname(__FILE__).'/../DAO/DAOTCC.php';
+
+
+//Criar TCC
+function criarTCC($Tcc)
+{
+	$DAOTCC = new DAOTCC();
+	$DAOTCC->create($Tcc);	
+}
+
+//Alterar Tcc
+function alterarTCC($Tcc,idTcc)
+{
+	$DAOTCC = new DAOTCC();
+	$DAOTCC->update($Tcc, $idTcc);	
+}
+
