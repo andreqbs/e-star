9b7d7ddf60215145d5e14e2d9d7df267df841440
lidio <lidio_sl@hotmail.com>
2016-11-10 21:38:00 -0300

    Tela de eventos está pronta.

---
+++ Public/eventos.php
@@ -0,0 +1,273 @@
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Eventos</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <link rel="stylesheet" href="estart/css/style.css">
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href="bower_components/AdminLTE/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="bower_components/AdminLTE/dist/css/AdminLTE.min.css">
+
+    <link rel="stylesheet" href="bower_components/AdminLTE/dist/css/skins/_all-skins.min.css">
+
+  </head>
+
+
+    
+
+    
+    <div class="-wrapper"><!-- Contém o conteúdo da página -->
+
+      
+      <section class="content-header"><!-- cabeçalho de conteúdo (cabeçalho da página) -->
+       <div class="box"><!-- Inicio do conteudo da tabela de notificações  -->
+       <div class="box-header "><!-- Inicio cabeçalho -->
+        <h1 class="box-title">
+          Eventos
+        </h1><!-- Fim do cabeçalho -->
+        
+          </div><!-- Fim do cabeçalho -->
+
+        <br>
+        <br>
+
+        
+
+                    <div class="container"><!-- Inicio da busca -->
+                      <form class="form-horizontal">  
+                        <div class="col-md-2">
+                          <label><h4>Buscar Evento</h4></label>
+                        </div>
+
+
+                          <div class="col-md-5">
+                            <input type="text" class="form-control"  placeholder="Palavra Chave">
+                          </div>
+                            <div class="col-md-2">
+                              <button class="btn btn-default" type="submit">
+                                <i class="glyphicon glyphicon-search">
+                                </i>
+                              </div>
+
+                                <div class="col-sm-3">
+                                  <button  type="button" class="btn btn-primary " data-toggle="modal" data-target="#myModal" >Novo item</button>
+                                  </button>
+                                </div>
+
+                                   
+
+                                                        </div>
+                        
+
+                      </form>
+                    </div><!-- Fim da busca -->
+
+
+                    <div class="container"><!-- Inicio do Filtro  -->
+                      <form class="form-horizontal"> 
+  </form>
+          </div> <!-- Fim do Filtro  -->
+                   
+  <div class="container"> </div>
+              <div class="box-body no-padding"><!-- Inicio da tabela  -->
+                <table class="table table-bordered table-hover table-responsive">
+                  <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Professor</th>
+                    <th>Local</th>
+                    <th>Data</th>
+
+                    <th style="text-align:center">Ação</th>
+
+                  </tr>
+                  </thead>
+                  <tbody>
+                  
+                  <tr>
+                    <td>1</td>
+                    <td>WorkShop</td>
+                    <td>AndreQBS </td>
+                    <td>UnP-Nascimento de Castro</td>
+                    <td>10/12/2016</td>           
+                    <td>
+                     <a href="#" class="btn btn-success btn-xs" data-toggle="modal" data-target="#myModalTCC"> Visualizar </a></button>
+                     <a href="#" class="btn btn-warning btn-xs" data-toggle="modal" data-target="#myModalTCC"> Editar </a></button>
+                      <a href="#" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#myModalTCC"> Excluir </a></button>
+                    </td>  
+                  </tr>
+                  <tr>
+                   <td>1</td>
+                    <td>WorkShop</td>
+                    <td>AndreQBS </td>
+                    <td>UnP-Nascimento de Castro</td>
+                    <td>10/12/2016</td>  
+                   <td>
+                     <a href="#" class="btn btn-success btn-xs" data-toggle="modal" data-target="#myModalTCC"> Visualizar </a></button>
+                     <a href="#" class="btn btn-warning btn-xs" data-toggle="modal" data-target="#myModalTCC"> Editar </a></button>
+                      <a href="#" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#myModalTCC"> Excluir </a></button>
+                    </td>  
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>WorkShop</td>
+                    <td>AndreQBS </td>
+                    <td>UnP-Nascimento de Castro</td>
+                    <td>10/12/2016</td>
+                    
+                
+                    <td>
+                     <a href="#" class="btn btn-success btn-xs" data-toggle="modal" data-target="#myModalTCC"> Visualizar </a></button>
+                     <a href="#" class="btn btn-warning btn-xs" data-toggle="modal" data-target="#myModalTCC"> Editar </a></button>
+                      <a href="#" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#myModalTCC"> Excluir </a></button>
+                    </td>  
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>WorkShop</td>
+                    <td>AndreQBS </td>
+                    <td>UnP-Nascimento de Castro</td>
+                    <td>10/12/2016</td>   
+                   <td>
+                     <a href="#" class="btn btn-success btn-xs" data-toggle="modal" data-target="#myModalTCC"> Visualizar </a></button>
+                     <a href="#" class="btn btn-warning btn-xs" data-toggle="modal" data-target="#myModalTCC"> Editar </a></button>
+                      <a href="#" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#myModalTCC"> Excluir </a></button>
+                    </td>  
+                  </tr>
+                 
+                  
+                  </tbody>
+
+                 
+                </table>
+                    <div class="box-footer clearfix">
+                      <ul class="pagination pagination-sm no-margin pull-right">
+                        <li><a href="#">&laquo;</a></li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">&raquo;</a></li>
+                      </ul>
+                    </div>
+              </div>
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    
+
+    </div><!-- Fim da tabela  -->
+
+    </div><!-- Fim do conteudo da tabela de notificações  -->
+
+    </section><!-- Fim da seção -->
+      
+    </div><!-- Fim do conteúdo da página -->
+    
+  </form>
+  </div><!--Fim do Conteudo-->
+
+
+
+
+                        <!-- Inicio da modal informações --> 
+                              <div class="modal fade" id="myModalTCC" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+
+                                <div class="modal-dialog " role="document">
+
+                                  <div class="modal-content">
+
+                                    <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel">Evento</h4>
+                                    </div>
+
+                                    <div class="modal-body">
+                                    
+                                        <div class="row"><!--Linha 01-->
+
+                                          <div class="col-xs-8 form-group">
+                                            
+                                              <label class="col-xs-1 control-label">Título:</label>
+                                              <input class="form-control" type="text" disabled="" placeholder="WorkShop">
+                                            
+                                            <!-- /.form-group -->
+                                          </div>
+                                          <!-- /.col -->
+                                          <div class="col-xs-3 form-group">
+
+                                            <label class="col-xs-4 control-label">Data:</label>
+                                            <input class="form-control" type="text" disabled="" placeholder="10/12/2016">
+                                
+                                          </div> 
+
+                                        </div><!--Fim Linha 01-->
+
+                                              
+                                        <div class="row"> <!--Linha 02-->
+                                              
+                                          <div class="col-xs-8 form-group">
+
+                                            <label class="col-xs-1 control-label">Professor:</label>
+
+                                            <input class="form-control" type="text" disabled="" placeholder="Andre Quitiliano">
+                                
+                                          </div>                                 
+                                                                          
+                                          <div class="col-xs-8 form-group">
+
+                                            <label class="col-xs-1 control-label">Local:</label>
+                                              <div class="col-xs">
+                                              <textarea class="form-control"  placeholder="UNP-NASCIMENTO DE CASTRO" disabled="" ></textarea>
+                                            </div>
+
+                                          </div>
+
+                                          <div class="col-xs-8 form-group">
+
+                                            <label class="col-xs-1 control-label">Observações:</label>
+                                              <div class="col-xs">
+                                              <textarea class="form-control"  placeholder="O evento será quando o Professor André QBS pagar a pizza" disabled="" ></textarea>
+                                            </div>
+
+                                          </div>
+
+
+                                        </div><!--Fim Linha 07-->
+
+                                    </div>
+
+
+                                  </div><!-- /.modal-content -->
+
+                                </div><!-- /.modal-dialog -->
+
+                              </div><!-- Fim da modal informações --> 
+                                      
+
+
+  <script src="bower_components/AdminLTE/plugins/jQuery/jquery-2.2.3.min.js"></script>
+
+  <script src="bower_components/AdminLTE/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="bower_components/AdminLTE/dist/js/app.min.js"></script>
+
+
+  </body>
+  </html>
