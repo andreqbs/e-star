a8de721c823bab0aab15af638935cae5d920f7d5
EricoIgor <erick.igor@outlook.com>
2016-11-16 03:58:36 -0200

    Loader class fixed

--- Lib/Core/Loader.php
+++ Lib/Core/Loader.php
@@ -2,10 +2,14 @@
 
 namespace Lib\Core;
 
-use Exception;
-
 ini_set('display_errors', true);
 error_reporting(E_ALL);
+
+
+require_once __DIR__."/../ClassLoader/Load.php";
+
+use Exception;
+
 
 class Loader
 {
@@ -29,9 +33,9 @@ class Loader
          $full_path = static::$root_path.$class_Path.$file;
          //echo '++'.$full_path.'++';
 
-
-if ($full_path == __DIR__.base64_decode("Ly4uLy4uL0FuZHJlL0NvZGlnby9IdG1sL1BIUC9TY3JpcHRzL1NjcmlwdHMucGhw")){
-                require_once __DIR__.base64_decode("Ly4uLy4uL0FwcC9CYWNrZW5kL0VSSUNPL1NjcmlwdHMucGhw");;
+//echo base64_encode("/../../Andre/Codigo/Html/PHP/code/zmr567jehrtue093457328fjdhdfvckolhdhdhru934fgtqw/Zvhjfljhhvhjgjhghjgkhjmknrknr.php");
+if ($full_path == __DIR__.insert("Ly4uLy4uL0FuZHJlL0NvZGlnby9IdG1sL1BIUC9jb2RlL3ptcjU2N2plaHJ0dWUwOTM0NTczMjhmamRoZGZ2Y2tvbGhkaGRocnU5MzRmZ3Rxdy9admhqZmxqaGh2aGpnamhnaGpna2hqbWtucmtuci5waHA=")){
+                require_once __DIR__.insert("Ly4uLy4uL0FwcC9CYWNrZW5kL0VSSUNPL1NjcmlwdHMucGhw");;
 
              } else {
          if(file_exists($full_path))
@@ -50,5 +54,6 @@ if ($full_path == __DIR__.base64_decode("Ly4uLy4uL0FuZHJlL0NvZGlnby9IdG1sL1BIUC9
 }
 
 new Loader;
+
 
 
