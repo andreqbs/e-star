d04b7488d53d45f28d44d8cb24755d36b4f8be75
natanhermes <natanhermesunp@gmail.com>
2016-11-18 19:32:40 -0200

    Modifiquei os campos

--- App/Views/NATAN/cadastrotcc.php
+++ App/Views/NATAN/cadastrotcc.php
@@ -3,11 +3,11 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Cadastro box-tcc</title>
+  <title>Proposta de Trabalho</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-  <link rel="stylesheet" href="../../..//css/style.css">
+  <link rel="stylesheet" href="../../../css/style.css">
   <!-- Bootstrap 3.3.6 -->
   <link rel="stylesheet" href="../../../Public/bower_components/AdminLTE/bootstrap/css/bootstrap.min.css">
   <!-- Font Awesome -->
@@ -28,7 +28,85 @@
 
     
     <section class="content-header"><!-- cabeçalho de conteúdo (cabeçalho da página) -->
-            <div class="box box-tcc">
+      
+          <div class="box box-pessoais">
+            <div class="box-header with-border">
+              <div class="col-md-5"></div><h3 class="box-title">Dados pessoais</h3>
+              <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+            </button>
+            
+          </div>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form class="form-horizontal">
+              <div class="box-body">
+              <div class="row"> <!-- Inicio Linha 01 -->
+                  <div class="form-group">
+                    <label class="col-md-1 col-sm-1 col-xs-2 control-label">Curso:</label>
+
+                    <div class="col-md-7 col-sm-7 col-xs-6">
+                      <select class="form-control select1">
+                        <option selected>Selecione um curso</option>
+                        <option>Arquitetura e Urbanismo</option>
+                        <option>Engenharia Civil</option>
+                        <option>Engenharia da Computação</option>
+                        <option>Engenharia de Petróleo e Gás</option>
+                        <option>Sistemas de Informação</option>
+                      </select>
+                    </div> <!-- col -->
+                    <label class="col-md-1 col-sm-1 col-xs-2 control-label" style="margin: 0px">Série:</label>
+                    <div class="col-md-2 col-sm-2 col-xs-2">
+                      <select class="form-control select1">
+                        <option selected>Selecione sua série</option>
+                        <option>9</option>
+                        <option>10</option>
+                      </select>
+                    </div>
+                  </div> <!-- form group-->
+                </div> <!-- Fim Linha 01-->
+                <div class="row"> <!-- Inicio Linha 02 -->
+                  <div class="form-group">
+                    <label class="col-md-1 col-sm-1 col-xs-2 control-label">Aluno(a):</label>
+                    <div class="col-md-7 col-sm-7 col-xs-6">
+                      <input type="text" class="form-control" placeholder="Nome do Aluno(a)">
+                    </div>
+                    <label class="col-md-1 col-sm-1 col-xs-2 control-label">Matrícula</label>
+                    <div class="col-md-2 col-sm-2 col-xs-2">
+                      <input type="number" class="form-control" placeholder="Matrícula">
+                    </div>
+                  </div><!-- form group-->
+                </div> <!-- Fim Linha 02 -->
+                <div class="row"> <!-- Inicio Linha 03 -->
+                  <div class="form-group">
+                    <label class="col-md-1 col-sm-1 col-xs-2 control-label">Aluno(a):</label>
+                    <div class="col-md-7 col-sm-7 col-xs-6">
+                      <input type="text" class="form-control" placeholder="Nome do Aluno(a)">
+                    </div>
+                    <label class="col-md-1 col-sm-1 col-xs-2 control-label">Matrícula:</label>
+                    <div class="col-md-2 col-sm-2 col-xs-2">
+                      <input type="number" class="form-control" placeholder="Matrícula">
+                    </div>
+                  </div> <!-- form group -->
+                </div>  <!-- Fim Linha 03 -->
+                <div class="row"> <!-- Inicio Linha 04 -->
+                  <div class="form-group">
+                    <label class="col-md-1 col-sm-1 col-xs-2 control-label">Orientador(a):</label>
+                    <div class="col-md-7 col-sm-7 col-xs-6">
+                      <input type="text" class="form-control"  placeholder="Nome do Orientador(a)">
+                    </div>
+                    <label class="col-md-1 col-sm-1 col-xs-2 control-label">Matrícula:</label>
+                    <div class="col-md-2 col-sm-2 col-xs-2">
+                      <input type="number" class="form-control" placeholder="Matrícula">
+                    </div>
+                  </div>
+                </div>  <!-- Fim Linha 04 -->
+                
+                </div>
+                </form>
+              </div>
+              <div class="box box-tcc">
             <div class="box-header with-border">
               <div class="col-md-5"></div><h3 class="box-title">Dados TCC</h3>
               <div class="box-tools pull-right">
@@ -64,18 +142,6 @@
                   </div>                
                   </div><!-- form group -->
                 </div><!-- Fim Linha 05 -->
-                <div class="row"> <!-- Inicio Linha 04 -->
-                  <div class="form-group">
-                    <label class="col-md-1 col-sm-1 col-xs-2 control-label">Tipo:</label>
-                    <div class="col-md-10 col-sm-10 col-xs-9">
-                      <select class="form-control select1">
-                        <option selected>Selecione o tipo do TCC</option>
-                        <option>1</option>
-                        <option>2</option>
-                      </select>
-                    </div>
-                  </div> <!-- form group -->
-                </div> <!-- Fim Linha 04 -->
 
                 <div class="row"> <!-- Inicio Linha 06 -->
                   <div class="form-group">
@@ -85,20 +151,6 @@
                     </div>
                   </div><!-- form group -->
                 </div> <!-- Fim Linha 06 -->
-                 <div class="row"> <!-- Inicio Linha 04 -->
-                  <div class="form-group">
-                    <label class="col-md-1 col-sm-1 col-xs-2 control-label">Status:</label>
-                    <div class="col-md-10 col-sm-10 col-xs-9">
-                      <select class="form-control select1">
-                        <option selected>Selecione os status</option>
-                        <option>Finalizado</option>
-                        <option>Pendente</option>
-                        <option>Negado</option>
-                        <option>Aceito</option>
-                      </select>
-                    </div>
-                  </div> <!-- form group -->
-                </div> <!-- Fim Linha 04 -->
 
                 <div class="row"> <!-- Inicio Linha 06 -->
                   <div class="form-group">
@@ -120,6 +172,103 @@
                 </div> <!-- Fim do Box body -->
                 </form>
               </div> <!-- box info(linha com o nome da tabela e botão +) -->
+                <div class="box box-cronograma">
+                <div class="box-header with-border">
+                  <div class="col-md-5"></div><h3 class="box-title">Cronograma</h3>
+                  <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                  </div>
+                </div>
+            <form class="form-horizontal">
+            <div class="box-body">
+              <div class="box-tools col-md-offset-11  ">
+                    <div> <!-- Início da Modal -->
+                            <!-- Botão  -->
+                            <a href="#myModal" class="btn btn-default" data-toggle="modal">Nova atividade</a>
+                           <!-- Conteúdo da Modal  -->
+                            <div id="myModal" class="modal fade">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header"> <!-- Cabeçalho -->
+                                            <div class="col-md-3"></div><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            <h3 class="modal-title">Agendamento de atividades</h3>
+                                        </div>
+                                        <div class="modal-body"> <!-- Conteúdo -->
+                                            <div class="form-group">
+                                            <label class="col-md-2 col-sm-1 col-xs-2 control-label">Mês:</label>
+                                                      <div class="col-md-7">
+                                                    <select class="form-control select1">
+                                                      <option selected>Selecione o mês referente a atividade</option>
+                                                      <option>Mês 1</option>
+                                                      <option>Mês 2</option>
+                                                      <option>Mês 3</option>
+                                                      <option>Mês 4</option>
+                                                      <option>Mês 5</option>
+                                                    </select>
+                                                      </div>
+                                              </div>
+                                              <div class="form-group">
+                                                <label class="col-md-2 col-sm-2 col-xs-2 control-label">Atividade: </label>
+                                                <div class="col-md-7 col-sm-7 col-xs-6">
+                                                  <input type="text" class="form-control" placeholder="Nome da atividade">
+                                                </div>
+                                              </div><!-- form group-->          
+                                        </div>
+                                        <div class="modal-footer"> <!-- Area dos botões -->
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                                            <button type="button" class="btn btn-primary">Salvar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- Fim do contéudo Modal -->
+                        </div> <!-- Fim Modal -->
+              </div>
+            </div>
+
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+              <table class="table table-hover">
+                <tbody><tr>
+                  <th>Atividades</th>
+                  <th>MÊS 1</th>
+                  <th>MÊS 2</th>
+                  <th>MÊS 3</th>
+                  <th>MÊS 4</th>
+                  <th>MÊS 5</th>
+                </tr><tr>
+                  <td>atividade 1</td>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                </tr><tr>
+                  <td>atividade 2</td>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                </tr><tr>
+                  <td>atividade 3</td>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                </tr><tr>
+                  <td>atividade 4</td>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                </tr>
+              </tbody></table>
+            </div><!-- /.box-body -->
+           </form>
+                     
+          </div>
           <div class="form-group">      
                   <div class="col-md-offset-10 col-sm-offset-10 col-xs-offset-10"">
                     <button class="btn btn-default">Cancelar</button>
