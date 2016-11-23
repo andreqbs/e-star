04cae4db30b73a1b80d673507131a572c6ba5fac
JFhenrique <jhsg2012@gmail.com>
2016-11-22 21:39:37 -0300

    ajuste4

---
+++ App/Views/JEFFERSON/Acompanhar.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>Acompanhar</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+  <link rel="stylesheet" href="estart/css/style.css">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="../../../Public/bower_components/AdminLTE/bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="../../../Public/bower_components/AdminLTE/dist/css/AdminLTE.min.css">
+
+  <link rel="stylesheet" href="../../../Public/bower_components/AdminLTE/dist/css/skins/_all-skins.min.css">
+
+</head>
+
+<body >
+
+
+      
+    <section class="content-header"><!-- cabeçalho de conteúdo (cabeçalho da página) -->
+      <div class="box"><!-- Inicio do conteudo da tabela de notificações  -->
+        <div class="box-header"><!-- Inicio cabeçalho -->
+          <h1 class="box-title">
+            Acompanhar  
+          </h1><!-- Fim do cabeçalho -->
+        
+        <div class="box-tools pull-right">
+          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+          </button>        
+        </div>
+
+        </div><!-- Fim do cabeçalho -->
+          
+        <br>
+        <br>
+
+        <div class="box-body no-padding"> <!-- Inicio da tabela  -->
+          <table id="Tabela1" class="table table-bordered table-hover table-responsive">
+            <thead>
+            <tr>
+              <th>O que?</th>
+              <th>Por que?</th>
+              <th>Onde?</th>
+              <th>Quando?</th>
+              <th>Quem?</th>
+              <th>Como?</th>
+              <th>Quanto?</th>
+            </tr>
+            </thead>
+            <tbody>
+            
+            <tr>
+              <td>Seminário orientacional</td>
+              <td>Apresentar Normas de TCC</td>
+              <td>Sala de aula</td>
+              <td>22 a 26/fev</td>
+              <td>Equipe Start-e</td>
+              <td>Slide Start-e</td>
+              <td>2h por grupo</td>
+            </tr>
+            <tr>
+              <td>Submissão proposta de trabalho</td>
+              <td>Formalizar proposta</td>
+              <td>e-Labora</td>
+              <td>21 a 25/mar</td>
+              <td>Alunos (com orientador)</td>
+              <td>Formulário Start-e</td>
+              <td>5d</td>
+            </tr>
+            <tr>
+              <td>Aprovação e termo de orientação</td>
+              <td>Formalizar orientação</td>
+              <td>e-Labora</td>
+              <td>28/mar a 01/abr</td>
+              <td>Orientador (com alunos)</td>
+              <td>Formulário Start-e</td>
+              <td>5d</td>
+            </tr>
+            <tr>
+              <td>Seminário metodológico</td>
+              <td>Reforçar normas de TCC</td>
+              <td>Sala de aula</td>
+              <td>28/mar a 01/abr</td>
+              <td>Equipe Start-e</td>
+              <td>Slide Start-e</td>
+              <td>2h por grupo</td>
+            </tr>
+            <tr>
+              <td>Relatório parcial c/ ficha de freq.</td>
+              <td>Avaliação U1</td>
+              <td>e-Labora</td>
+              <td>18 a 22/abr</td>
+              <td>Aluno</td>
+              <td>Formulário Start-e + TCC</td>
+              <td>5d</td>
+            </tr>
+            <tr>
+              <td>Avaliação parcial</td>
+              <td>Avaliação U1</td>
+              <td>e-Labora</td>
+              <td>06 a 10/jun</td>
+              <td>Aluno</td>
+              <td>Formulário Start-e + TCC</td>
+              <td>5d</td>
+            </tr>
+            <tr>
+              <td>Seminário metodológico</td>
+              <td>Apoio metodológico</td>
+              <td>Sala de aula</td>
+              <td>25 a 29/abr</td>
+              <td>Orientador</td>
+              <td>Formulário Start-e</td>
+              <td>5d</td>
+            </tr>
+            <tr>
+              <td>Relatório final c/ ficha de freq.</td>
+              <td>Avaliação U2</td>
+              <td>e-Labora</td>
+              <td>02 a 06/mai</td>
+              <td>Equipe Start-e</td>
+              <td>Slide Start-e</td>
+              <td>2h por grupo</td>
+            </tr>
+            <tr>
+              <td>Avaliação final</td>
+              <td>Avaliação U2</td>
+              <td>e-Labora</td>
+              <td>13 a 24/jun</td>
+              <td>Orientador</td>
+              <td>Formulário Start-e</td>
+              <td>10d</td>
+            </tr>
+            
+            </tbody>
+
+           
+          </table>
+
+        </div><!-- Fim da tabela  -->       
+          
+      </div><!-- Fim do conteudo da tabela de notificações  -->
+
+    </section><!-- Fim da seção -->
+
+
+<script src="../../../Public/bower_components/AdminLTE/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="../../../Public/bower_components/AdminLTE/dist/js/app.min.js"></script>
+
+
+</body>
+</html>
