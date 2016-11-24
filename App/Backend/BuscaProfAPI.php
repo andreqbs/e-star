49d1e87e4b90f761d3a850a5ce5b68af7c7a7b9f
Rômulo Santos <romulosantos11@hotmail.com>
2016-11-24 18:37:47 -0300

    teste de DAO

--- App/Backend/BuscaProfAPI.php
+++ App/Backend/BuscaProfAPI.php
@@ -7,19 +7,27 @@ session_start();
 
 use App\Models\Professor as Professor;
 use App\DAO\DAOProfessor as DAOProfessor;
+
 require_once dirname(__FILE__).'/../Model/Professor.php';
 require_once dirname(__FILE__).'/../DAO/DAOProfessor.php';
 
 //INSERT
-//$meuProfessor = new Professor("TalkingHand", "Luva Foda", "1", "Fazer uma luva foda!!", "Pq eh massa!!!", "1", "1");
+$meuProfessor = new Professor("TalkingHand", "123456", "teste@teste.com.br", "foda", "Pq eh massa!!!");
+//`sp_inserirProfessor`(nome varchar(100), matricula char(6), email varchar(50), login varchar(50), senha varchar(20))
 
 
 $searchfield = $_POST['searchfield'];
 $type = $_POST['type'];
 
-$meuDAOProfessor = new DAOProfessor();
-$Result =$meuDAOProfessor->listBy($type, $searchfield);
+$Professortest = new mainController();
+$Result =$Professortest->cadastrarProfessor($meuProfessor);
 
+if ($Result){
+	
+		echo '< script> alert("cadastrado com sucesso") </script>  ';
+}	
+else 
+	echo '< script> alert("cadastrado com sucesso") </script>  ';
 
 
 
