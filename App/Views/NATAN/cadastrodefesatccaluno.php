eddbb0c416cf2113fd8498bd8822aa112519ca3f
natanhermes <natanhermesunp@gmail.com>
2016-11-25 04:21:30 -0200

    Finalizada tela de cadastro da defesa de TCC do aluno. Algumas modificações na tela do professor.

--- App/Views/NATAN/cadastrodefesatccaluno.php
+++ App/Views/NATAN/cadastrodefesatccaluno.php
@@ -1,204 +1,136 @@
 <!DOCTYPE html>
 <html>
 <head>
+
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Cadastro Defesa TCC </title>
-  <!-- Tell the browser to be responsive to screen width -->
+  <title>Defesa TCC Aluno</title>
+
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
   <link rel="stylesheet" href="../../../css/style.css">
-  <!-- Bootstrap 3.3.6 -->
+  
   <link rel="stylesheet" href="../../../Public/bower_components/AdminLTE/bootstrap/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
 
   <link rel="stylesheet" href="../../../Public/bower_components/AdminLTE/dist/css/AdminLTE.min.css">
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+
   <link rel="stylesheet" href="../../../Public/bower_components/AdminLTE/dist/css/skins/_all-skins.min.css">
 
-</head>
-<body class="">
+  <link rel="stylesheet" href="../../../Public/bower_components/AdminLTE/plugins/datepicker/datepicker3.css">
 
-<div class="container-fluid"><!--Conteudo-->  
-<div class="content"><!-- Contém o conteúdo da página -->
-
-    
-    <section class="content-header"><!-- cabeçalho de conteúdo (cabeçalho da página) -->
-      <br>
-      <br>
-          <div class="box box-pessoais">
-            <div class="box-header with-border">
-              <div class="col-md-5"></div><h3 class="box-title">Defesa do TCC</h3>
-              <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-            </button>
-            
-          </div>
-            </div>
-            <!-- /.box-header -->
-            <!-- form start -->
-            <form class="form-horizontal">
-              <div class="box-body">
-              <div class="col-md-3"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Título TCC:</label>
-                    <div class="col-md-4">
-                      <input type="text" class="form-control" placeholder="Título do TCC" readonly="true">
-                    </div>
-                  </div><!-- form group-->
-                </div> <!-- Fim Linha 01 -->
-              <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Postura pessoal:</label>
-                    <div class="col-md-6">
-                      <textarea class="form-control" style="resize: none" placeholder="Escreva aqui sua postura"></textarea>
-                    </div>
-                  </div><!-- form group-->
-                </div> <!-- Fim Linha 01 -->
-                <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Fluência Comunicativa:</label>
-                    <div class="col-md-6">
-                      <textarea class="form-control" style="resize: none" placeholder="Escreva aqui sua postura"></textarea>
-                    </div>
-                  </div><!-- form group-->
-                </div>
-                <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Clareza:</label>
-                    <div class="col-md-6">
-                      <textarea class="form-control" style="resize: none" placeholder="Escreva aqui sua postura"></textarea>
-                    </div>
-                  </div><!-- form group-->
-                </div>
-               <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Domínio:</label>
-                    <div class="col-md-6">
-                      <textarea class="form-control" style="resize: none" placeholder="Escreva aqui sua postura"></textarea>
-                    </div>
-                  </div><!-- form group-->
-                </div>
-                <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Nota final:</label>
-                    <div class="col-md-6">
-                       <input type="number" class="form-control" placeholder="Nota de avaliação em relação a defesa">
-                    </div>
-                  </div><!-- form group-->
-                </div>                
-               </div>
-              </form>
-            </div>
-            <div class="box box-tcc">
-            <div class="box-header with-border">
-              <div class="col-md-4"></div><h3 class="box-title">Comentários sobre a Defesa do TCC</h3>
-              <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-            </button>
-              </div>
-            </div><!-- /.box-header -->
-
-            <form class="form-horizontal">
-              <div class="box-body">
-                <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Postura pessoal:</label>
-                    <div class="col-md-6">
-                      <textarea class="form-control" style="resize: none" placeholder="Comentários sobre a postura pessoal"></textarea>
-                    </div>
-                  </div><!-- form group-->
-                </div>
-                <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Fluência Comunicativa:</label>
-                    <div class="col-md-6">
-                      <textarea class="form-control" style="resize: none" placeholder="Comentários sobre a Fluência Comunicativa"></textarea>
-                    </div>
-                  </div><!-- form group-->
-                </div>
-                <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Clareza:</label>
-                    <div class="col-md-6">
-                      <textarea class="form-control" style="resize: none" placeholder="Comentários sobre a clareza"></textarea>
-                    </div>
-                  </div><!-- form group-->
-                </div>
-                <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Domínio:</label>
-                    <div class="col-md-6">
-                      <textarea class="form-control" style="resize: none" placeholder="Comentários sobre o domínio do tema"></textarea>
-                    </div>
-                  </div><!-- form group-->  
-                </div>
-                <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Data:</label>
-                    <div class="col-md-6">
-                      <input type="date" class="form-control" placeholder="Data e horário da banca">
-                    </div>
-                  </div><!-- form group-->
-                </div> <!-- Fim Linha 01 -->
-                <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Título do TCC:</label>
-                    <div class="col-md-6">
-                      <input type="text" class="form-control" placeholder="Título do TCC">
-                    </div>
-                  </div><!-- form group-->
-                </div> <!-- Fim Linha 01 -->
-                <div class="col-md-2"></div>
-                <div class="row"> <!-- Inicio Linha 01 -->
-                  <div class="form-group">
-                    <label class="col-md-1 control-label">Sala:</label>
-                    <div class="col-md-6">
-                      <input type="number" class="form-control" placeholder="Reserva de sala">
-                    </div>
-                  </div><!-- form group-->
-                </div> <!-- Fim Linha 01 -->
-                </div> <!-- Fim do Box body -->
-                </form>
-              </div> <!-- box info(linha com o nome da tabela e botão +) -->  
-           <br/>           
-          </div>
-          <div class="form-group">      
-                  <div class="col-md-offset-10">
-                    <button class="btn btn-default">Cancelar</button>
-                    <button class="btn btn-primary">Salvar</button>
-                  </div>
-          </div><!-- form group  button-->
-        
-    </section><!-- Fim da seção -->
-    
-  </div><!-- Fim do conteúdo da página -->
   
 
+</head>
 
-</div><!--Fim do Conteudo-->
 
 
+<body>
+
+<div class="container=fluid"> 
+<div class="content">    
+        <section class="content-header">
+          <div class="box box-pessoais">
+
+            <div class="box-header with-border">
+              
+              <h3 class="box-title">Defesa de TCC</h3>
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <form>
+              <div class="box-body" >
+                                                 
+                          <div class="row"> 
+                                        <div class="col-md-10">
+                                        <label class="control-label">Título TCC:</label>
+                                          <input type="text" class="form-control" placeholder="Título do TCC" disabled="">
+                                        </div>
+                          </div>
+                          <div class="row"> 
+                                        <div class="col-md-8">
+                                        <label>Aluno(a): </label>
+                                        <input type="text" class="form-control"  placeholder="Titulo do TCC">
+                                        </div>
+                                        <div class="col-md-2">
+                                        <label>Matrícula: </label>
+                                        <input type="number" class="form-control">
+                                        </div>
+                          </div>
+                          <div class="row">
+                                        <div class="col-md-8">
+                                        <label>Postura / Comentário:</label>
+                                         <textarea class="form-control"  style="resize: none"  placeholder="Enter ..."></textarea>
+                                        </div>
+                                        <div class="col-md-2">
+                                        <label>Nota da postura: </label>
+                                        <input type="number" class="form-control">
+                                        </div>
+                         </div>
+                          <div class="row"> 
+                                        <div class="col-md-8">
+                                        <label>Comunicação / Comentário:</label>
+                                         <textarea class="form-control"  style="resize: none"  placeholder="Enter ..."></textarea>
+                                        </div>
+                                        <div class="col-md-2">
+                                        <label>Nota da comunicação: </label>
+                                        <input type="number" class="form-control">
+                                        </div>
+                          </div>
+                          <div class="row"> 
+                                        <div class="col-md-8">
+                                        <label>Clareza / Comentário:</label>
+                                         <textarea class="form-control"  style="resize: none"  placeholder="Enter ..."></textarea>
+                                        </div>
+                                        <div class="col-md-2">
+                                        <label>Nota da clareza: </label>
+                                        <input type="number" class="form-control">
+                                        </div>
+                          </div>
+                          <div class="row"> 
+                                        <div class="col-md-8">
+                                        <label>Domínio / Comentário:</label>
+                                         <textarea class="form-control"  style="resize: none"  placeholder="Enter ..."></textarea>
+                                        </div>
+                                        <div class="col-md-2">
+                                        <label>Nota do domínio: </label>
+                                        <input type="number" class="form-control">
+                                        </div>
+                          </div>
+                          <div class="row"> 
+                                        <div class="col-md-8"></div>
+                                        <div class="col-md-2">
+                                        <label>Nota total: </label> 
+                                        <input type="number" class="form-control" placeholder="Soma das notas" disabled=""> 
+                                        </div>
+                          </div>
+
+              </div><!-- /.box-body -->
+            </form> 
+          </div><!-- /.box geral-->
+
+          <div class="form-group">      
+             <div class="col-md-offset-10">
+              <button class="btn btn-default">Cancelar</button>
+              <button class="btn btn-primary">Salvar</button>
+              </div>
+          </div><!-- form group  button-->
+        </section>
+
+</div> <!-- Fim content -->
+</div> <!-- Fim container fluid -->
 
 <script src="../../../Public/bower_components/AdminLTE/plugins/jQuery/jquery-2.2.3.min.js"></script>
-
+<!-- Bootstrap 3.3.6 -->
 <script src="../../../Public/bower_components/AdminLTE/bootstrap/js/bootstrap.min.js"></script>
 
 <script src="../../../Public/bower_components/AdminLTE/dist/js/app.min.js"></script>
-
+<!-- bootstrap datepicker -->
+<script src="../../../Public/bower_components/AdminLTE/plugins/datepicker/bootstrap-datepicker.js"></script>
 
 </body>
 </html>
